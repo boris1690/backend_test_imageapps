53ad8277747f553804df00b2b79ec8aa47a4f544
Boris <boris_90@hotmail.es>
2023-09-29 14:49:14 -0500

    add feature signup

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to application." });
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
