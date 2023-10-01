b497dd82c33d67b04f58d737fab5dcf32d5d5100
Boris <boris_90@hotmail.es>
2023-10-01 12:52:24 -0500

    fix urls

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+var os = require('os');
+
+function getIPAddress() {
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+  
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+          return alias.address;
+      }
+    }
+    return '0.0.0.0';
+}
 
 const app = express();
 
@@ -33,4 +48,8 @@ require('./app/routes/task.routes')(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+  
+  console.log(`====== API URL TO DEVICE: ${getIPAddress()}:${PORT} =====================`);
 });
+
+
